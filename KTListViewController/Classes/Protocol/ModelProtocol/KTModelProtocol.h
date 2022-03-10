b2764db06a2c8c63643d5b98f144f866b0168fbe
ling <lingyj2014@gmail.com>
2022-03-10 09:59:56 +0800

    opz

--- KTListViewController/Classes/Protocol/ModelProtocol/KTModelProtocol.h
+++ KTListViewController/Classes/Protocol/ModelProtocol/KTModelProtocol.h
@@ -9,6 +9,7 @@
 #define KTModelProtocol_h
 
 #define KTSynthesizeReuseViewModelProtocol 	@synthesize reuseViewClassName = _reuseViewClassName;\
+											@synthesize isSelected = _isSelected;\
 											@synthesize isFirstItem = _isFirstItem;\
 											@synthesize isLastItem = _isLastItem;\
 
@@ -26,6 +27,7 @@
 @optional
 
 @property (nonatomic, copy, nullable) NSString *reuseViewClassName;
+@property (nonatomic, assign) BOOL isSelected;
 @property (nonatomic, assign) BOOL isFirstItem;
 @property (nonatomic, assign) BOOL isLastItem;
 
